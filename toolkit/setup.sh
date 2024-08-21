59684f891ec9999651a3e8751020ad7796fed61b
Daniel McIlvaney <damcilva@microsoft.com>
2024-08-21 14:32:49 -0700

    more debug

--- toolkit/setup.sh
+++ toolkit/setup.sh
@@ -2,35 +2,41 @@
 
 set -ex
 
-basedir=/temp/DockerStage/
-chrootdir=$basedir/docker-chroot-1
-repo_dir=/home/damcilva/repos/temp/CBL-Mariner_TEMP3
+script_dir=$(dirname $0)
+workdir=$(pwd)/mariner_workdir
+sudo rm -rf $workdir
 
-#sudo rm -rf $basedir
-#mkdir -p $chrootdir
+chroot_dir=/temp/DockerStage/
+basedir=$chroot_dir/docker-chroot-
+repo_dir=/home/mariner_user/CBL-Mariner
 
-#touch $basedir/chroot-pool.lock
-
-# Mounts to create for each chroot (we only have 1 for now...)
-# Use array to make it easier to add more mounts in the future
 mounts=()
+mounts+=(-v $workdir:/tmp/mariner)
 mounts+=(-v $repo_dir:/repo)
-mounts+=(-v /dev:$chrootdir/dev:ro  )
-mounts+=(-v /proc:$chrootdir/proc:ro  )
-mounts+=(-v devpts:$chrootdir/dev/pts:ro )
-mounts+=(-v sysfs:$chrootdir/sys:ro )
-mounts+=(-v $repo_dir/out/RPMS/noarch:$chrootdir/localrpms/noarch:rw  )
-mounts+=(-v $repo_dir/out/RPMS/x86_64:$chrootdir/localrpms/x86_64:rw  )
-mounts+=(-v $repo_dir/out/RPMS/aarch64:$chrootdir/localrpms/aarch64:rw  )
-mounts+=(-v $repo_dir/build/rpm_cache/cache:$chrootdir/upstream-cached-rpms:rw  )
-mounts+=(-v $repo_dir/build/toolchain_rpms/x86_64:$chrootdir/toolchainrpms/x86_64:rw  )
-mounts+=(-v $repo_dir/build/toolchain_rpms/aarch64:$chrootdir/toolchainrpms/aarch64:rw  )
-mounts+=(-v $repo_dir/build/toolchain_rpms/noarch:$chrootdir/toolchainrpms/noarch:rw )
+
+num_chroots=10
+for i in $(seq 1 $num_chroots); do
+    chrootdir=$basedir$i
+    mounts+=(-v /dev:$chrootdir/dev:ro  )
+    mounts+=(-v /proc:$chrootdir/proc:ro  )
+    mounts+=(-v devpts:$chrootdir/dev/pts:ro )
+    mounts+=(-v sysfs:$chrootdir/sys:ro )
+    mounts+=(-v $workdir/out/RPMS/noarch:$chrootdir/localrpms/noarch:rw  )
+    mounts+=(-v $workdir/out/RPMS/x86_64:$chrootdir/localrpms/x86_64:rw  )
+    mounts+=(-v $workdir/out/RPMS/aarch64:$chrootdir/localrpms/aarch64:rw  )
+    mounts+=(-v $workdir/build/rpm_cache/cache:$chrootdir/upstream-cached-rpms:rw  )
+    mounts+=(-v $workdir/build/toolchain_rpms/x86_64:$chrootdir/toolchainrpms/x86_64:rw  )
+    mounts+=(-v $workdir/build/toolchain_rpms/aarch64:$chrootdir/toolchainrpms/aarch64:rw  )
+    mounts+=(-v $workdir/build/toolchain_rpms/noarch:$chrootdir/toolchainrpms/noarch:rw )
+done
+
+# cd to docker build folder and ensure image is up to date
+docker build $script_dir/engdocker -t mcr.microsoft.com/azurelinux/local/buildcontainer
 
 docker run  --privileged -it --rm -v $repo_dir:/repo "${mounts[@]}" mcr.microsoft.com/azurelinux/local/buildcontainer:latest bash -c "\
     git config --global --add safe.directory /repo && \
-    touch $basedir/chroot-pool.lock && \
+    touch $chroot_dir/chroot-pool.lock && \
     bash --rcfile <(echo 'cd /repo/toolkit') \
 "
 
-#sudo make build-packages SPECS_DIR=../SPECS_TEST DAILY_BUILD_ID=lkg REBUILD_TOOLS=y CHROOT_DIR=/temp/DockerStage/ build-packages LOG_LEVEL=debug
+#sudo make build-packages SPECS_DIR=../SPECS_TEST DAILY_BUILD_ID=lkg REBUILD_TOOLS=y CHROOT_DIR=/temp/DockerStage/ build-packages LOG_LEVEL=debug BUILD_DIR=/tmp/mariner/build OUT_DIR=/tmp/mariner/out -j10
